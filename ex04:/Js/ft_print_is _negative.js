3e47ac27ca0b475beb25adb179515cbe5e522268
Thibaudrab <ajmiaou@gmail.com>
2022-12-04 20:40:36 +0100

    repo exercice4 bouton N ou P

---
+++ ex04:/Js/ft_print_is _negative.js
@@ -0,0 +1,13 @@
+function afficherSigne() {
+   
+    var n = prompt("Entrez un entier :");
+    if (n < 0) {
+      document.write('N');
+    } 
+  else {
+  document.write('P');
+    }
+  }
+  
+    
+  
